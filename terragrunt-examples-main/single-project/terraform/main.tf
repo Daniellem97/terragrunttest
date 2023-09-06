151c11e3ac84ccfe0726cff6e6821f2d0cf3f136
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-09-06 22:04:37 +0100

    Update main.tf

--- terragrunt-examples-main/single-project/terraform/main.tf
+++ terragrunt-examples-main/single-project/terraform/main.tf
@@ -5,12 +5,24 @@ resource "random_pet" "this" {
 
 resource "aws_instance" "dev_node" {
     instance_type = "t2.large"
+  ami = data.aws_ami.server_ami.id
 
 
     root_block_device {
         volume_size = 10
     }
 }
+
+data "aws_ami" "server_ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+}
+
 resource "aws_s3_bucket" "exampletestingtginfracost" {
   bucket = "exampletestingtginfracost"
 
