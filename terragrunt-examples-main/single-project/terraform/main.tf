ad1e6629e1d4b112a60472ebd29f1044247706c7
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-09-06 22:03:26 +0100

    Update main.tf

--- terragrunt-examples-main/single-project/terraform/main.tf
+++ terragrunt-examples-main/single-project/terraform/main.tf
@@ -1,3 +1,35 @@
 resource "random_pet" "this" {
   length = 5
 }
+
+
+resource "aws_instance" "dev_node" {
+    instance_type = "t2.large"
+
+
+    root_block_device {
+        volume_size = 10
+    }
+}
+resource "aws_s3_bucket" "exampletestingtginfracost" {
+  bucket = "exampletestingtginfracost"
+
+  tags = {
+    Name        = "exampletestingtginfracost"
+    Environment = "Dev"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0" # Consider specifying a version constraint for better stability.
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "default"
+}
