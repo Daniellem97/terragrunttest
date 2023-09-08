afe95aec620e1a37c19bd54ebb606f80a017e45c
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-09-08 11:52:24 +0100

    Update main.tf

--- terragrunt-examples-main/multi-project/config3/main.tf
+++ terragrunt-examples-main/multi-project/config3/main.tf
@@ -1,30 +1,19 @@
-provider "aws" {
-  region = "eu-west-1"
+resource "aws_instance" "dev_node" {
+    instance_type = "t2.large"
+  ami = data.aws_ami.server_ami.id
+
+
+    root_block_device {
+        volume_size = 10
+    }
 }
 
-terraform {
-  backend "s3" {}
-}
+data "aws_ami" "server_ami" {
+  most_recent = true
+  owners      = ["099720109477"]
 
-resource "aws_security_group" "this" {
-  name   = "this"
-  vpc_id = var.vpc_id
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
-
-  tags = {
-    Name  = "security_group1"
-    env   = "dev"
-    owner = "config3"
-  }
-}
-
-variable "vpc_id" {
-  description = "The ID of the VPC"
-  type        = string
 }
