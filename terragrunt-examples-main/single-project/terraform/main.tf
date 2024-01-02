03aac6013d205528ce429b305748cfd9212fc537
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-01-02 13:28:49 +0000

    Update main.tf

--- terragrunt-examples-main/single-project/terraform/main.tf
+++ terragrunt-examples-main/single-project/terraform/main.tf
@@ -1,3 +1,18 @@
+
+provider "aws" {
+  region = "us-west-2" # You can choose the region that suits your needs
+}
+
 resource "random_pet" "this" {
   length = 5
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
