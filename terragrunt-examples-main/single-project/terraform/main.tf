2894251e228ae1cee9cec969c43e154eca470a6d
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-04-09 10:55:55 +0100

    Update main.tf

--- terragrunt-examples-main/single-project/terraform/main.tf
+++ terragrunt-examples-main/single-project/terraform/main.tf
@@ -7,8 +7,148 @@ resource "random_pet" "this" {
   length = 5
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "mybucketbiyfhdp" # Bucket names must be unique across all existing bucket names in Amazon S3
+resource "aws_s3_bucket" "my_bucket0" {
+  bucket = "mybucketbiyfhdp0" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket1" {
+  bucket = "mybucketbiyfhdp1" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket2" {
+  bucket = "mybucketbiyfhdp2" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket3" {
+  bucket = "mybucketbiyfhdp3" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket4" {
+  bucket = "mybucketbiyfhdp4" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket5" {
+  bucket = "mybucketbiyfhdp5" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket6" {
+  bucket = "mybucketbiyfhdp6" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket7" {
+  bucket = "mybucketbiyfhdp7" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket8" {
+  bucket = "mybucketbiyfhdp8" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket9" {
+  bucket = "mybucketbiyfhdp9" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket10" {
+  bucket = "mybucketbiyfhdp10" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket11" {
+  bucket = "mybucketbiyfhdp11" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket12" {
+  bucket = "mybucketbiyfhdp12" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket13" {
+  bucket = "mybucketbiyfhdp13" # Bucket names must be unique across all existing bucket names in Amazon S3
+  acl    = "private" # Access control list
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket14" {
+  bucket = "mybucketbiyfhdp14" # Bucket names must be unique across all existing bucket names in Amazon S3
   acl    = "private" # Access control list
 
   tags = {
