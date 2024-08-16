d9dd558a3674debd87cd962c83bba46ed9c2daaf
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-08-16 19:03:58 +0200

    Update main.tf

--- terragrunt-examples-main/multi-project/config1/main.tf
+++ terragrunt-examples-main/multi-project/config1/main.tf
@@ -1,16 +1,38 @@
+terraform {
+  required_providers {
+    spacelift = {
+      source  = "spacelift.io/spacelift-io/spacelift"
+      version = "1.13.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "spacelift" {}
+
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-west-2"  # You can change the region as needed
 }
 
 terraform {
   backend "s3" {}
 }
 
-resource "aws_vpc" "this" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-      Name  = "vpc1"
-      env   = "dev"
-      owner = "config1"
-    }
+resource "spacelift_stack" "this" {
+ github_enterprise {
+    namespace = "Daniellem97"
+  }
+
+branch = "main"
+name = "labelsdebug"
+repository = "securitygroups"
+labels            = var.spacelift_stack_labels
+}
+
+variable "spacelift_stack_labels" {
+  description = "Stack labels"
+  type        = set(string)
 }
