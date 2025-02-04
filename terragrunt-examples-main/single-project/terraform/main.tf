5766d0909ee9df11f5315cfb18e29905b4e036b0
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-02-04 13:09:58 +0000

    Update main.tf

--- terragrunt-examples-main/single-project/terraform/main.tf
+++ terragrunt-examples-main/single-project/terraform/main.tf
@@ -22,7 +22,7 @@ resource "random_pet" "this" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "mybg8787gip" # Bucket names must be unique across all existing bucket names in Amazon S3
+  bucket = "mybggip" # Bucket names must be unique across all existing bucket names in Amazon S3
   acl    = "private" # Access control list
 
   tags = {
